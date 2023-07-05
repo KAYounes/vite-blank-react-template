1a54cbbbdf669f7e8e05297eee5f08a97a19922b
Kareem Amr Younes <official.kyounes@gmail.com>
2023-07-05 23:29:04 +0300

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.module.css'
+
+if (import.meta.hot) {
+  import.meta.hot.on("vite:beforeUpdate", () => console.clear());
+}
+
+function App() {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default App
